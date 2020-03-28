b28cdd30a9149c612daffacfd4f336f9cc0b2d51
KristapsD <49512575+KristapsD@users.noreply.github.com>
2020-03-28 20:57:19 +0200

    finished form

--- js/main.js
+++ js/main.js
@@ -2,11 +2,19 @@
 var modal = document.getElementById("myModal");
 
 var btn = document.getElementById("buttonbuy");
+var btn1 = document.getElementById("buttonbuy1");
+var btn2 = document.getElementById("buttonbuy2");
 
 var span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
-  modal.style.display = "block";
+    modal.style.display = "block";
+}
+btn1.onclick = function() {
+    modal.style.display = "block";
+}
+btn2.onclick = function() {
+    modal.style.display = "block";
 }
 
 span.onclick = function() {
@@ -18,3 +26,29 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+const menuIcon = document.getElementById("menu-icon");
+const slideoutMenu = document.getElementById("slideout-menu");
+const searchIcon = document.getElementById("search-icon");
+const searchBox = document.getElementById("searchbox");
+
+searchIcon.addEventListener('click', function () {
+    if(searchBox.style.top == '72px'){
+        searchBox.style.top = '24px';
+        searchBox.style.pointerEvents = 'none';
+    } else {
+        searchBox.style.top = '72px';
+        searchBox.style.pointerEvents = 'auto';
+    }
+
+})
+
+menuIcon.addEventListener('click', function () {
+    if(slideoutMenu.style.opacity == '1'){
+        slideoutMenu.style.opacity = '0';
+        slideoutMenu.style.pointerEvents = 'none';
+    } else {
+        slideoutMenu.style.opacity = '1';
+        slideoutMenu.style.pointerEvents = 'auto';
+    }
+})
