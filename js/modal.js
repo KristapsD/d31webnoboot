8b95b5fadab509f1a5d1aac8cefae54be466ce8e
KristapsD <49512575+KristapsD@users.noreply.github.com>
2020-03-29 22:33:18 +0300

    fixed burg menu

---
+++ js/modal.js
@@ -0,0 +1,30 @@
+var modal = document.getElementById("myModal");
+
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("buttonbuy");
+var btn1 = document.getElementById("buttonbuy1");
+var btn2 = document.getElementById("buttonbuy2");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+btn1.onclick = function() {
+    modal.style.display = "block";
+}
+btn2.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
